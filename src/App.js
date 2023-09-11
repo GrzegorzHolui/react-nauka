da743957bbbebfc9417f042647cf530e2147bc7e
Grzegorz <grzesiu.holui@gmail.com>
2023-09-11 19:59:22 +0200

    learning what component is

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from "react-dom/client";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Przycisk - po kliknięciu dodowania jest kolejna literka
+
+
+class App extends React.Component {
+
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         text: "",
+    //     }
+    //     this.handleClick = this.handleClick.bind(this);
+    // }
+
+    state = {
+        text: ""
+    }
+
+    handleClick = () => {
+        const number = Math.floor(Math.random() * 10)
+        this.setState({
+            text: this.state.text + number
+        })
+    }
+
+    // trzeba dodać bind do this w miejscu w którym
+    // jest uruchamiana funkcja
+
+    // można również zwracać funkcję która ma przypisanego do argumentu poprzednika this
+    handleClick() {
+        const letter = "a";
+        this.setState((prevState) => ({
+                text: this.state.text + letter
+            })
+        )
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <button onClick={this.handleClick}>Dodaj Cyfrę</button>
+                <PanelResult text={this.state.text}/>
+            </React.Fragment>
+        )
+    }
 }
+
+const PanelResult = (props) => {
+    return (
+        <h1>{props.text}</h1>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
 
 export default App;
